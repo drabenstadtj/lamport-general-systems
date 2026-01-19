52ad969a6e857c0c5dfbaf0dc74ee5f82440e5b9
Jack Drabenstadt <jackdrabenstadt@gmail.com>
2026-01-19 17:02:38 -0500

    Fixed misplaced sticky notes, added exit prompt

--- lamport-general-systems/scenes/prefabs/player/player.gd
+++ lamport-general-systems/scenes/prefabs/player/player.gd
@@ -93,6 +93,10 @@ func _connect_signals() -> void:
 # INPUT HANDLING
 
 func _handle_free_input(event: InputEvent) -> void:
+	if event.is_action_pressed("close"):
+		get_tree().quit()
+		return
+	
 	if event.is_action_pressed("ui_cancel"):
 		toggle_mouse_mode()
 		return
