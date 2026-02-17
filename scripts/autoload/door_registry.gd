e281a310d61c81be4117734713ad7a88c69d74ec
Jack Drabenstadt <jackdrabenstadt@gmail.com>
2026-02-17 14:21:32 -0500

    fixing layer fighting on transfer room doors

--- scripts/autoload/door_registry.gd
+++ scripts/autoload/door_registry.gd
@@ -1,10 +1,7 @@
 extends Node
 
 var docks: Dictionary = {}
-<<<<<<< Updated upstream
-=======
 var spawned_transfer_rooms: Dictionary = {}
->>>>>>> Stashed changes
 
 func register_dock(dock_name: String, dock_node: Node3D) -> void:
 	docks[dock_name] = dock_node.global_transform
@@ -16,5 +13,12 @@ func get_dock(dock_name: String) -> Transform3D:
 func has_dock(dock_name: String) -> bool:
 	return docks.has(dock_name)
 
+func register_transfer_room(dock_name: String, transfer_room: Node3D) -> void:
+	spawned_transfer_rooms[dock_name] = transfer_room
+
+func has_transfer_room(dock_name: String) -> bool:
+	return spawned_transfer_rooms.has(dock_name)
+
 func clear() -> void:
 	docks.clear()
+	spawned_transfer_rooms.clear()
